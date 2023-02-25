fbf93af43c8bb2b398ea163acb17aae8666aa175
Xx_JetBlackSalvation_xX <91695842+jetblacksalvation@users.noreply.github.com>
2023-02-24 17:46:53 -0800

    Update main.asm

--- MasmTest/main.asm
+++ MasmTest/main.asm
@@ -1,41 +1,56 @@
-includelib	kernel32.lib
-includelib	msvcrt.lib
+includelib kernel32.lib	
+includelib msvcrt.lib
 
+.data	
 
-.data
-string db"hello world",0
 .code
-stringAddrs	equ	<[rbp-8]>
-
-stringLength equ <[rbp-16]>
-externdef	WriteConsoleA :proto		
 externdef	GetStdHandle:proto	
-	main proc
+externdef	WriteConsoleA :proto	
+
+stringAddrs	equ	<rbp-8>
+
+stdout equ <rbp-16>
+	puts	proc
 	push rbp	
 	mov rbp, rsp
-	sub rsp, 40
-	lea rcx, string
-	mov stringAddrs,rcx	;convenient local variable addressing 
-	; for element in string addrs inc stringLength if not 0
-	xor rcx,rcx
-	LoopStart:
-	cmp dword ptr[stringAddrs + rcx], 0
-	jz LoopEnd
-	inc rcx
+	sub rsp, 70h
+	;double address
+	mov qword ptr [stringAddrs], rcx	
+	mov rdx, qword ptr [stringAddrs]
 
-	jmp LoopStart
-	LoopEnd:
-	mov stringLength, rcx
+	mov bl, byte ptr [rdx]
 	mov rcx, -11
 	call GetStdHandle
-	mov rcx, rax; DWORD hStdOut = GetStdHandle(-11);
-	lea rdx, stringAddrs
-	mov r8, stringLength
+	; rax now has handle to stdout move that to stdout
+	mov qword ptr [stdout], rax
+	xor r8,r8
+	;Looping until terminating character
+	LabelStart:
+	; use r8 to count
+	cmp	 byte ptr[rdx+r8], 0
+	jz LabelEnd
+	inc r8
+	jmp LabelStart
+	LabelEnd:
+	;write to console
+	mov rcx, qword ptr[stdout]
 	call WriteConsoleA
-
 
 	mov rsp,rbp	;rsp is non-volitie, make sure to restore it 
 	pop rbp	
-	ret		
+	ret	
+	puts endp	
+
+	string db "amongus",10,0
+
+	main proc	
+	sub rsp, 70h
+	mov rcx, offset string
+	call puts	
+	add rsp,70h
+
+	ret	
 	main endp	
+
+
 	end
